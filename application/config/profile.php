50b03ef525241392a6f0bc38175beef2e19f57dd
king <king@host.docker.internal>
2021-08-30 22:33:31 +0800

    update admin demo

--- application/config/profile.php
+++ application/config/profile.php
@@ -16,16 +16,16 @@ $profile = [];
 /**
  * 基本设置
  */
-//$profile['debug'] = TRUE;      /*是否开启调试模式: bool FALSE 不开启 | bool TRUE 开启*/
+$profile['debug']['enabled'] = TRUE;      /*是否开启调试模式: bool FALSE 不开启 | bool TRUE 开启*/
 $profile['timezone'] = 'PRC';  /*设置时区*/
 $profile['charset'] = 'utf-8'; /*设置编码*/
 
 /**
  * 异常模块
  */
-$profile['exception']['enable'] = TRUE;  /*异常处理:bool TRUE 开启异常统一处理|bool FALSE 屏蔽所有异常|string log 异常为日志输出*/
+$profile['exception']['enabled'] = TRUE;  /*异常处理:bool TRUE 开启异常统一处理|bool FALSE 屏蔽所有异常|string log 异常为日志输出*/
 $profile['exception']['log'] = TRUE;     /*是否以日志方式输出*/
-$profile['exception']['logid'] = 'tiny_exception';
+$profile['exception']['logid'] = 'tinyphp_exception';
 
 /**
  * 应用设置 配置文件里的相对路径 都是基于application文件夹所在路径
@@ -34,34 +34,34 @@ $profile['app']['namespace'] = 'App';   /*命名空间*/
 $profile['app']['resources'] = 'resource/';  /*资源文件夹*/
 $profile['app']['runtime'] = 'runtime/';
 $profile['app']['tmp'] = 'runtime/tmp/';
-
+//$profile['app']['cached']['enabled'] = TRUE;
 /**
  * 自动加载引导类
  */
-$profile['bootstrap']['enable'] = FALSE;
+$profile['bootstrap']['enabled'] = FALSE;
 $profile['bootstrap']['class'] = '\App\Common\Bootstarp';
 
 /**
  * 打包器设置
  */
-$profile['build']['enable'] = TRUE;  /*不开启时 忽略build打包行为*/
+$profile['build']['enabled'] = TRUE;  /*不开启时 忽略build打包行为*/
 $profile['build']['param_name'] = 'build'; /*--build参数 开启打包工作*/
 $profile['build']['plugin'] = '\Tiny\MVC\Plugin\Builder';
 $profile['build']['path'] = 'build/builder'; /*打包配置文件夹*/
 $profile['build']['setting_path'] = 'build/setting';  /*打包器的设置文件夹，用来自定义application.config数据*/
-$profile['build']['profile_path'] = 'build/profile';  /*打包器的属性文件夹,用来自定义application.proerties数据*/
+$profile['build']['profile_path'] = 'build/profile';  /*打包器的属性文件夹,用来自定义application.properties数据*/
 
 /**
  * 调试器设置
  */
-$profile['debug']['enable'] = TRUE;
+//$profile['debug']['enable'] = TRUE;
 $profile['debug']['param_name'] = 'debug';
 $profile['debug']['class'] = '\Tiny\MVC\Plugin\Debug';
 
 /**
  * 守护进程的基本设置
  */
-$profile['daemon']['enable'] = TRUE;
+$profile['daemon']['enabled'] = TRUE;
 $profile['daemon']['id'] = 'tinyphp-daemon';          /*默认的daemonid*/
 $profile['daemon']['plugin'] = '\Tiny\MVC\Plugin\Daemon';
 $profile['daemon']['piddir'] = 'runtime/pid/'; /*守护进程pid目录*/
@@ -73,11 +73,12 @@ $profile['daemon']['tick'] = 2;                /*检测子进程退出后的tick
  *                     worker 运行指定次数退出的worker
  *                     timerworker 定时触发的worker 未实现
  *                     networker 监听各种端口的worker 未实现
-*/
+ */
 $profile['daemon']['policys'] = [
-    'tinyd-demo' => [
+    'tinyphp-daemon' => [
         'workers' => [      //worker子进程配置
-            ['id' => 'index', 'type' => 'worker' , 'args' => ['controller' => 'main', 'action' => 'index'], 'num' => 3, 'options' => ['runmax' => 1024, 'interval' => '0.1']]
+            ['id' => 'index', 'type' => 'worker' , 'args' => ['controller' => 'main', 'action' => 'index'], 'num' => 1, 'options' => ['runmax' => 1024, 'tick' => '0.1']],
+            ['id' => 'test', 'type' => 'worker' , 'args' => ['controller' => 'main', 'action' => 'test'], 'num' => 10, 'options' => ['runmax' => 1024, 'tick' => '1']]
         ],
         'options' => [],
     ],
@@ -103,9 +104,7 @@ $profile['src']['view'] = 'views/';             /*视图源码*/
 $profile['config']['enabled'] = TRUE;   /* 是否开启默认配置模块 */
 $profile['config']['path'] = 'config/'; /* 配置文件相对路径 */
 $profile['config']['paths'] = [];       /*可加载多个扩展的配置文件或文件夹路径，必须为绝对或者相对路径 数据可覆盖*/
-$profile['config']['cache']['enable'] = TRUE; /*配置模块缓存设置 提高性能*/
-$profile['config']['cache']['id'] = 'default';
-$profile['config']['cache']['ttl'] = 60;
+$profile['config']['cache']['enabled'] = TRUE; /*配置模块缓存设置 提高性能*/
 
 /**
  * 语言模块设置
@@ -113,7 +112,9 @@ $profile['config']['cache']['ttl'] = 60;
 $profile['lang']['enabled'] = TRUE;   /*是否开启 */
 $profile['lang']['locale'] = 'zh_cn';
 $profile['lang']['path'] = 'lang/';   /*存放语言包的目录 */
-
+$profile['lang']['cache']['enabled'] = TRUE; /*配置模块缓存设置 提高性能*/
+$profile['lang']['cache']['id'] = 'default';
+$profile['lang']['cache']['ttl'] = 60;
 /**
  * 日志模块设置
  */
@@ -133,12 +134,12 @@ $profile['log']['path'] = 'runtime/log/';
 $profile['data']['enabled'] = TRUE;    /* 是否开启数据池 */
 $profile['data']['charset'] = 'utf8';
 $profile['data']['policys'] = [
+    ['id' => 'default', 'driver' => 'db.mysql_pdo', 'host' => '127.0.0.1', 'port' => '3306', 'user' => 'root', 'password' => '123456', 'dbname' => 'mysql'],
     ['id' => 'redis', 'driver' => 'redis', 'host' => '127.0.0.1', 'port' => '6379' ],
     ['id' => 'redis_cache', 'driver' => 'redis', 'host' => '127.0.0.1', 'port' => '6379', 'servers' => [['host' => '127.0.0.1', 'port' => '6379'],['host' => '127.0.0.1', 'port' => '6379']]],
-	['id' => 'redis_session', 'driver' => 'redis', 'host' => '127.0.0.1', 'port' => '6379'],
+    ['id' => 'redis_session', 'driver' => 'redis', 'host' => '127.0.0.1', 'port' => '6379'],
     ['id' => 'redis_queue', 'driver' => 'redis', 'host' => '127.0.0.1', 'port' => '6379'],
-    ['id' => 'memcached', 'driver' => 'memcached', 'host' => '127.0.0.1', 'port' => '11211'],
-    ['id' => 'default', 'driver' => 'db.mysql_pdo', 'host' => '127.0.0.1', 'port' => '3306', 'user' => 'root', 'password' => '123456', 'dbname' => 'mysql']
+    ['id' => 'memcached', 'driver' => 'memcached', 'host' => '127.0.0.1', 'port' => '11211']
 ];
 
 /**
@@ -150,10 +151,10 @@ $profile['data']['policys'] = [
  * 类型 5 Redis缓存 */
 $profile['cache']['enabled'] = TRUE; /* 是否默认开启缓存模块，若不开启，则以下设置无效 */
 $profile['cache']['lifetime'] = 3600;
-$profile['cache']['path'] = 'runtime/cache/'; /* 缓存文件夹相对路径 */
+$profile['cache']['filepath'] = 'runtime/cache/'; /*文件缓存方式的缓存相对路径*/
 $profile['cache']['policys'] = [
     ['id' => 'default', 'driver' => 'redis', 'lifetime' => 3600, 'dataid' => 'redis_cache'],
-    ['id' => 'def', 'driver' => 'file', 'lifetime' => 3600, 'path' => '']
+    ['id' => 'file', 'driver' => 'file', 'lifetime' => 3600, 'path' => '']
 ];
 
 /**
@@ -246,7 +247,7 @@ $profile['router']['enabled'] = TRUE; /* 是否开启router */
 $profile['router']['routers'] = []; /*注册自定义的router*/
 $profile['router']['rules'] = [
     ['router' => 'pathinfo', 'rule' => ['ext' => '.html'], 'domain' => ''],
-    ];
+];
 
 /**
  * 是否开启插件
@@ -257,44 +258,44 @@ $profile['plugin']['enabled'] = FALSE;
  * 需要添加绝对路径的相对路径
  */
 $profile['path'] = [
-            'src.path',
-            'app.assets',
-            'build.path',
-            'build.profile_path',
-            'build.setting_path',
-            'config.path',
-            'lang.path',
-            'log.path',
-            'cache.path',
-            'view.src',
-            'view.cache',
-            'view.compile',
-            'view.config',
-            'src.library',
-            'src.global',
-			'src.controller',
-			'src.console',
-			'src.rpc',
-			'src.model',
-			'src.common',
-            'daemon.piddir',
-            'daemon.logdir'
+    'src.path',
+    'app.assets',
+    'build.path',
+    'build.profile_path',
+    'build.setting_path',
+    'config.path',
+    'lang.path',
+    'log.path',
+    'cache.path',
+    'view.src',
+    'view.cache',
+    'view.compile',
+    'view.config',
+    'src.library',
+    'src.global',
+    'src.controller',
+    'src.console',
+    'src.rpc',
+    'src.model',
+    'src.common',
+    'daemon.piddir',
+    'daemon.logdir'
 ];
 
 /**
  * 自动加载库的配置
  */
 $profile['imports'] = [
-		'App\Controller' => 'src.controller',
-		'App\Controller\Console' => 'src.console',
-		'App\Controller\Rpc' => 'src.rpc',
-		'App\Model' => 'src.model',
-		'App\Common' => 'src.common',
-		'*' => 'src.global',
+    'App\Controller' => 'src.controller',
+    'App\Controller\Console' => 'src.console',
+    'App\Controller\Rpc' => 'src.rpc',
+    'App\Model' => 'src.model',
+    'App\Common' => 'src.common',
+    '*' => 'src.global',
 ];
 
 /**
  * 是否需要替换加载库里的路径为真实路径
  */
- $profile['import_no_replacepath'] = FALSE;
+$profile['import_no_replacepath'] = FALSE;
 ?>
---
+++ application/models/Admin.php
@@ -0,0 +1,17 @@
+<?php 
+/**
+ *
+ * @copyright (C), 2013-, King.
+ * @name Admin.php
+ * @author King
+ * @version stable 1.0
+ * @Date 2021年8月30日下午9:52:51
+ * @Description
+ * @Class List 1.
+ * @Function List 1.
+ * @History King 2021年8月30日下午9:52:51 第一次建立该文件
+ *          King 2021年8月30日下午9:52:51 修改
+ *         
+ */
+
+?>
