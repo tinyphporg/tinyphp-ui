72371b1e09ab66a075f1e21c58edc06d9ecf39c9
a123456 <a123456@graciedeimac>
2021-09-24 20:33:51 +0800

    added

--- application/build-front/src/lib/TinyPHP.js
+++ application/build-front/src/lib/TinyPHP.js
@@ -4,30 +4,42 @@ import '@lib/bootstrap';
 import AdminLTE from '@lib/adminlte';
 import Plugin from '@lib/tinyphp/Plugin';
 
+class TinyPHP {
 
-const TinyPHP = function(...arg){
-    return new Plugin(...arg);
-}
-
-TinyPHP.merge = Prototype.merge;
-
-TinyPHP.Plugin = Plugin;
-
-TinyPHP.ProtoType = Prototype;
-
-TinyPHP.AdminLTE = AdminLTE;
-
-
-TinyPHP._JQueryExtends = {
-    alert: alert
-}
-
-TinyPHP._JQueryFnExtends = {
+    static _window = window;
     
+    static _$ = $;
+    
+    static Prototype = Prototype;
+
+    static AdminLTE = AdminLTE;
+    
+    static Plugin  = Plugin;
+    
+    static merge = Prototype.merge;
+
+    static extend = Prototype.extend;
+    
+    static alert = alert;
+    
+    static _JQueryExtends = {
+        alert: alert
+    }
+    
+    static _JQueryFnExtends = {
+        
+    }
+    
+    static _extendJQuery() {
+        TinyPHP._$.extend(TinyPHP._JQueryExtends);
+    }
+    
+    static _init() {
+        TinyPHP._extendJQuery();
+    }
+
 }
 
-/* JQuery API */
-$.extend(TinyPHP._JQueryExtends);
-$.fn.extend(TinyPHP._JQueryFnExtends);
+TinyPHP._init();
 
 export default TinyPHP;
